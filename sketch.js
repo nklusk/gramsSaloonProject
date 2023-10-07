81f500321d59d082932603231bbebbddeac069fb
NATALIE LUSK <NKL22@fsu.edu>
2023-10-06 22:50:53 -0400

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+let saloonEntrance;
+let dialogue1;
+
+function preload() {
+  saloonEntrance = loadImage("assets/gramsSaloonEntrance.png");
+  dialogue1 = loadImage("assets/gramDialogue1.png");
+  dialogue2 = loadImage("assets/gramDialogue2.png");
+}
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(900, 632);
+  image(saloonEntrance, 0, 0);
 }
 
 function draw() {
-  background(125);
+  // background(125);
+}
+
+function mousePressed() {
+  image(dialogue1, 0, 0); 
+}
+
+function mousePressed() {
+  image(dialogue2, 0, 0);
 }
