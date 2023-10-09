11ca651bdb1db863302e721aa741b7eef58712fb
NATALIE LUSK <NKL22@fsu.edu>
2023-10-09 01:25:46 -0400

    stopping point

--- ATMinterface.js
+++ ATMinterface.js
@@ -4,6 +4,8 @@ let randomAd;
 let watchAd;
 let displayImage;
 let totalAttens;
+let displayAttens;
+
 
 function preload() {
 
@@ -13,25 +15,39 @@ function preload() {
 
 
 function setup() {
+backToGram = createButton("back to gram")
 watchAd = createButton("watch ad");
 totalAttens = 0
   createCanvas(900, 632);
   image(ATMinterface, 0, 0);
   
-  watchAd.mouseClicked(clicked);
-
+  
+  watchAd.mouseClicked(watchAdclicked);
+    backToGram.mouseClicked(backToGramClicked);
 }
 
 
 function draw() {
-  // background(125);
+  
   watchAd.position(98, 240);
   watchAd.size(346, 89);
+backToGram.size(346,89);
+backToGram.position(96, 345);
 }
 
-function clicked(){
+function watchAdclicked(){
+    // fill('black');
+    rect(250, 500, 100, 100)
+ displayAttens = createP(totalAttens);
+  displayAttens.position(260, 518);
+    displayAttens.style('color', 'black');
+
     image(randomAd, 506, 67);
-    console.log(clicks);
+    console.log(totalAttens);
+    totalAttens += 1
 
 }
   
+function backToGramClicked(){
+    location.assign('secondDialogue.html');
+}
